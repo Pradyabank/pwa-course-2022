24bcac2bbf8cbbc9e668696fef56c86f73a4f8d9
pradyabank <pradchaya.chal@bumail.net>
2022-01-22 16:58:51 +0700

    WORK 1 ALL

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function helloworld() {
-  alert("BOOM!");
+function Click1() {
+  alert("MY CHAT");
+}
+
+function Click2() {
+  alert("ข้อความ");
+}
+
+function Click3() {
+  alert("พิมพ์ข้อความ");
 }
